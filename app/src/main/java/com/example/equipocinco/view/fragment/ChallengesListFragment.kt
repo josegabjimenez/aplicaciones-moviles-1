7975e6582c04c8e84aec4f130c85d251fe317cb5
Cesar-velasquez117 <velasquez.cesar@correounivalle.edu.co>
2023-11-10 21:19:56 -0500

    Toolbar implemented

---
+++ app/src/main/java/com/example/equipocinco/view/fragment/ChallengesListFragment.kt
@@ -0,0 +1,34 @@
+package com.example.equipocinco.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.equipocinco.R
+import androidx.appcompat.widget.Toolbar
+import android.widget.TextView
+import com.example.equipocinco.databinding.FragmentChallengesListBinding
+
+class ChallengesListFragment : Fragment() {
+    private lateinit var binding: FragmentChallengesListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentChallengesListBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    private fun setupToolbar(toolbar: Toolbar) {
+        val toolbarTitle = toolbar.findViewById<TextView>(R.id.toolbarTitle)
+        toolbarTitle.text = "Retos"
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
